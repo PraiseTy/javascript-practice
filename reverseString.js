8acb2b4071c9841dff4e86a97a59a44a192413e6
Praise <praisetoyo@gmail.com>
2023-08-25 15:29:40 -0700

    Challenge five

---
+++ reverseString.js
@@ -0,0 +1,12 @@
+const reverseString = str => {
+    let a = ""
+    let b = ""
+    for(let i = str.length-1; i >= 0; i--){
+        a = str[i].toLowerCase();
+        if (a >="a" && a<= "z"){
+            b += a
+        }
+      }
+      return b;
+    }
+console.log(reverseString("Hello, I’m Jappy-Lappy-Happy"))
